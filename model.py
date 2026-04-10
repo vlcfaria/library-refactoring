13ebac6d1f0bd9555f7936c0a5e1889b531de1dd
Vinicius Faria <artixsproductions@gmail.com>
2026-04-10 16:19:32 -0300

    Commit 7: Moving method get_frequent_renter_points from Rental to Book

--- model.py
+++ model.py
@@ -24,6 +24,12 @@ class Book:
                 amount += (days_rented - 3) * 1.5
         
         return amount
+    
+    def get_frequent_renter_points(self, days_rented):
+        points = 1
+        if self.price_code == Book.NEW_RELEASE and days_rented > 1:
+            points += 1
+        return points
 
 class Rental:
     def __init__(self, book: Book, days_rented: int):
@@ -34,10 +40,7 @@ class Rental:
         return self.book.get_charge(self.days_rented)
 
     def get_frequent_renter_points(self):
-        points = 1
-        if self.book.price_code == Book.NEW_RELEASE and self.days_rented > 1:
-            points += 1
-        return points
+        return self.book.get_frequent_renter_points(self.days_rented)
 
 class Client:
 
